fb3f9feb63993dbad9b51038df95fbcdf283a85c
Maria <mariafcknt@outlook.com>
2026-09-10 22:51:18 -0300

    exercicio propriedades

---
+++ 06-propriedades-exercicios/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Titulo from './Titulo'
+
+function Home() {
+  return (
+    <section>
+      <Titulo texto="Home" />
+      <p>Essa é a home</p>
+    </section>
+  )
+}
+
+export default Home
--- 06-propriedades/src/Form/Button.jsx
+++ 06-propriedades/src/Form/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Button(props) {
-  console.log(props);
   return <button>Clique</button>;
 }
 
